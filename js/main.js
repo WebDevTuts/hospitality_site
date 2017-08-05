4a7d36b16619da5eb709d858118872fc4e615045
edwinchen85 <edwinchen85@gmail.com>
2017-08-05 17:48:23 +0800

    Add main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var hotelInfo;
+var details;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "data.json", true);
+xhr.responseType = 'text';
+xhr.send();
+
+xhr.onload = function() {
+    if(xhr.status === 200) {
+        hotelInfo = JSON.parse(xhr.responseText);
+    }
+}
